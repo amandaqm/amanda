ae7dfae1c8be245671a18a9ce5186151dfeb711e
Choi-Woo-Young <success7895@gmail.com>
2017-08-20 17:58:12 +0900

    test

--- app/src/main/java/kr/co/niceinfo/qm/amanda/AmandaApp.java
+++ app/src/main/java/kr/co/niceinfo/qm/amanda/AmandaApp.java
@@ -32,7 +32,7 @@ import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
  * Created by janisharali on 27/01/17.
  */
 
-// wychoi1111111111111ww
+// wychoi1
 public class AmandaApp extends Application {
 
     @Inject
