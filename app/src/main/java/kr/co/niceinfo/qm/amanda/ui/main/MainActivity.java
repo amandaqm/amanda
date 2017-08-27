68cbc306a3767da9c268bfc7e3069fb44860439b
Choi-Woo-Young <success7895@gmail.com>
2017-08-27 14:28:28 +0900

    main

--- app/src/main/java/kr/co/niceinfo/qm/amanda/ui/main/MainActivity.java
+++ app/src/main/java/kr/co/niceinfo/qm/amanda/ui/main/MainActivity.java
@@ -3,19 +3,27 @@ package kr.co.niceinfo.qm.amanda.ui.main;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import kr.co.niceinfo.qm.amanda.R;
+import kr.co.niceinfo.qm.amanda.ui.base.AmandaView;
+import kr.co.niceinfo.qm.amanda.ui.base.BaseActivity;
 import kr.co.niceinfo.qm.amanda.ui.login.LoginActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements AmandaView, MainMenuFragment.OnFragmentInteractionListener{
+
+    private static final String TAG = "MainActivity";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,5 +73,17 @@ public class MainActivity extends AppCompatActivity {
     public static Intent getStartIntent(Context context) {
         Intent intent = new Intent(context, MainActivity.class);
         return intent;
+    }
+
+    //BaseActivity
+    @Override
+    protected void setUp() {
+
+    }
+
+    //MainMenuFragment.OnFragmentInteractionListener
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        Log.i(TAG, "onFragmentInteraction Uri:" + uri.toString());
     }
 }
