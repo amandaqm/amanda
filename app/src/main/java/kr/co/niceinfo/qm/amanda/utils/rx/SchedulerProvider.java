707fe912e38ecb64eac1d6003447babf6d6bf409
Choi-Woo-Young <success7895@gmail.com>
2017-09-15 00:07:49 +0900

    Notice List

---
+++ app/src/main/java/kr/co/niceinfo/qm/amanda/utils/rx/SchedulerProvider.java
@@ -0,0 +1,17 @@
+package kr.co.niceinfo.qm.amanda.utils.rx;
+
+import io.reactivex.Scheduler;
+
+/**
+ * Created by hrskrs on 5/8/2017.
+ */
+
+public interface SchedulerProvider {
+
+    Scheduler ui();
+
+    Scheduler computation();
+
+    Scheduler io();
+
+}
